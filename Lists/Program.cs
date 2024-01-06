496c40a77af212f810c1011262ccdc3fff00e259
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-01-06 05:30:20 -0600

    Learned List

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -1,3 +1,20 @@
 ﻿
 
-List<>
+List<string> firstName = new List<string>();
+
+firstName.Add("Tim");
+firstName.Add("Sue");
+firstName.Add("Bob");
+firstName.Add("Jane");
+
+Console.WriteLine(firstName[firstName.Count - 1]);
+
+List<int> age = new List<int>();
+age.Add(1);
+age.Add(2);
+age.Add(3);
+
+string data = "Smith, Jones, Lee";
+List<string> lastName = data.Split(',').ToList();
+
+lastName.Add("Koo");
